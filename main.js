dc13d80082a2d296ba0ce2fe765b856a95419c4b
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2022-07-13 03:33:09 -0500

    touchmove

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
 const image2 = document.querySelector(".image2");
 
-window.addEventListener('mousemove', e => {
+window.addEventListener('touchmove', e => {
   image2.style.left = e.clientX + 'px';
 });
 
-window.addEventListener('touchstart', e => {
-  image2.style.left = e.clientX + 'px';
-});
+
 
 const image4 = document.querySelector(".image4");
 
-window.addEventListener('mousemove', e => {
+window.addEventListener('touchmove', e => {
   image4.style.left = e.clientX + 'px';
 });
 
-window.addEventListener('touchstart', e => {
-  image4.style.left = e.clientX + 'px';
-});
