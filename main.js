79ebba247eec78b410e061471ff5f03f946014ab
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2022-07-13 02:11:41 -0500

    webb's first images

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ const image2 = document.querySelector(".image2");
 window.addEventListener('mousemove', e => {
   image2.style.left = e.clientX + 'px';
 });
+
+const image4 = document.querySelector(".image4");
+
+window.addEventListener('mousemove', e => {
+  image4.style.left = e.clientX + 'px';
+});
